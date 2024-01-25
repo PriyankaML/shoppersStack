0ca3619ff8936e0354e021cefb16e8852de3b737
Windows <Windows@DESKTOP-O6PK2DD>
2024-01-25 11:27:21 +0530

    Class has been added

---
+++ src/test/java/executionTest/Package/com/InitiateClass.java
@@ -0,0 +1,18 @@
+package executionTest.Package.com;
+
+import org.testng.annotations.Test;
+
+public class InitiateClass {
+	
+	@Test
+	public void installPackage()
+	{
+		System.out.println("Packages has been installed");
+	}
+	@Test
+	public void readyPackage()
+	{
+		System.out.println("Packages are ready");
+	}
+
+}
